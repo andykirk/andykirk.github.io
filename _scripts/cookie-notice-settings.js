4ae8cace325a102a252d2e9948dc730da871933b
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2016-04-01 16:31:28 +0100

    First draft

---
+++ _scripts/cookie-notice-settings.js
@@ -0,0 +1,21 @@
+/*------------------------------------------------------------------------------------------------*\
+    Fall-Back Cookie Notice Pattern v0.1
+    ------------------------------------
+    
+    To avoid any confusion, it's probably best to copy these settings to another file that you're
+    concatenating and then make any changes to the defaults.
+\*------------------------------------------------------------------------------------------------*/
+
+var cookie_name                   = 'andykirk_accept_cookies';
+var cookie_expire_days            = 60;
+var cookie_notice_id              = 'cookie_notice';
+var cookie_button_id              = 'accept_cookies';
+var cookie_notice_class           = 'cookie_notice';
+var cookie_button_class           = '';
+var cookie_close_class            = 'cookie_notice--close';
+var cookie_notice_effect_duration = 1000;
+var cookie_html                   = 
+'<div id="' + cookie_notice_id + '" class="' + cookie_notice_class + '">' + "\n" +
+'<p class="cookie_notice__message">This site uses <a href="http://www.allaboutcookies.org/" rel="external" target="_blank">cookies</a> to improve user experience. By using this site you agree to our use of cookies.</p>' + "\n" +
+'<span class="cookie_notice__action"><button id="' + cookie_button_id + '" class="' + cookie_button_class + '">Dismiss</button></span>' + "\n" +
+'</div>';
